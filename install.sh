d6d86542c0b0380b4f410689475d3b1f578edc2f
Atops93 <aetops93@proton.me>
2024-08-22 16:52:37 +0930

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
-sudo pacman -S feh xorg xorg-server xorg-xinit
-mv .xinitrc ~/atops/
-mv .config/i3 ~/.config/
-mv .config/polybar ~/.config/
-mv .config/fastfetch ~/.config/
-mv .config/glava ~/.config/
-mv .config/rofi ~/.config/
+echo Do you want to install polybar? (y/n)
+if [[y]]; then
+	sudo pacman -S polybar
+
+sudo pacman -S feh i3 xorg xorg-server xorg-xinit
+sudo mv .xinitrc ~/atops/
+sudo mv .config/i3 ~/.config/
+sudo mv .config/polybar ~/.config/
+sudo mv .config/fastfetch ~/.config/
+sudo mv .config/glava ~/.config/
+sudo mv .config/rofi ~/.config/
 cd st && sudo make clean install
